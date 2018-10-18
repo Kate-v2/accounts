cea18bd6488b050c8508acf57f6c59082b2e5241
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-17 19:33:02 -0600

    login method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ApplicationRecord
   validates_presence_of :name, :password
   validates :email, presence: true, uniqueness: true
 
+  def self.authenticate(email, password)
+    # if email and password correspond to a valid user, return that user
+    # otherwise, return nil
+  end
+
 
 end
